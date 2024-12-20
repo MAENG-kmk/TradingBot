7d19f8e66340582c19b8dd010e9a49d2c48c8b1d
Kim mingi <kmk2737@snu.ac.kr>
2024-12-20 13:43:12 +0900

    edit error

--- logics/closePosition.py
+++ logics/closePosition.py
@@ -10,7 +10,7 @@ def closePosition(client, createOrder, positions, position_info, getBalance, sen
         createOrder(client, position['symbol'], 'BUY', 'MARKET', position['amount'])
       info = position_info[position['symbol']]
       position_info.pop(position['symbol'], None)
-      balance, _ = float(getBalance(client))
-      message += "symbol: {}, ror: {:.2f}%, profit: {:.2f}$ balance: {:.2f} \n side: {}, entered rsi: {} \n\n".format(position['symbol'], position['ror'], position['profit'], balance, info[0], info[1])
+      balance, _ = getBalance(client)
+      message += "symbol: {}, ror: {:.2f}%, profit: {:.2f}$ balance: {:.2f} \n side: {}, entered rsi: {} \n\n".format(position['symbol'], position['ror'], position['profit'], float(balance), info[0], info[1])
   if len(message) > 0:
     asyncio.run(send_message(message))
