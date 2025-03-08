2e461ac1a4b04c08d5864e6b94d4020e1635e7e4
Kim mingi <kmk2737@snu.ac.kr>
2025-03-08 20:25:53 +0900

    edit decide position

--- logics/decidePosition.py
+++ logics/decidePosition.py
@@ -1,15 +1,15 @@
 def decidePosition(ticker, BTC_data, getMa):
-  percent = sum(ticker['value'])/sum(ticker['quoteVolume'])
+  # percent = sum(ticker['value'])/sum(ticker['quoteVolume'])
   ma = getMa(BTC_data)
-
-  if percent > 0:
-    return 'long'
+  return ma
+  # if percent > 0:
+  #   return 'long'
     # if ma == 'long':
     #   return 'long'
     # else:
     #   return 'None'
-  else:
-    return 'short'
+  # else:
+  #   return 'short'
     # if ma == 'short':
     #   return 'short'
     # else:
