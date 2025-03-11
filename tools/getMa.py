8d4806be5611f4fee1f59ec2359e0782830b4be3
Kim mingi <kmk2737@snu.ac.kr>
2025-03-11 22:12:24 +0900

    hot fix

--- tools/getMa.py
+++ tools/getMa.py
@@ -1,12 +1,11 @@
 def getMa(data):
   closes = []
-  short_period = 5
-  long_period = 14
-  for close in data.iloc[-27:]['Close']:
+  short_period = 7
+  long_period = 20
+  for close in data.iloc[-20:]['Close']:
     closes.append(float(close))
   short_ma = sum(closes[-short_period:]) / short_period
   long_ma = sum(closes[-long_period:]) / long_period
-
   if short_ma > long_ma:
     return 'long'
   else:
